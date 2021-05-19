25c4d0870c3cef29cc6ac294786a7611691d9570
Marwa Ghassa <marwaghassa7@gmail.com>
2021-05-19 13:16:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(array, cb) {
+  let r = []
+  for (let i = 0; i < array.length; i++) {
+    r.push(cb(array[i]))
+  }
+
+  return r;
+}
+
+function reduce(array, cb, starting){
+  let r = (!!starting) ? starting : array[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < array.length; i++) {
+    r = cb(array[i], r)
+  }
+
+  return r;
+}
