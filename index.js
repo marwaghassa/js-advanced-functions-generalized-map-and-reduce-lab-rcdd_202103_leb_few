101ec274c44c9501f8bfba829e28161c86856584
Marwa Ghassa <marwaghassa7@gmail.com>
2021-05-19 13:05:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(array, cb) {
+  let r = []
+  for (let i = 0; i < array.length; i++) {
+    r.push(cb(array[i]))
+  }
+
+  return r;
+}
+
+function reduce(src, cb, starting){
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = cb(src[i], r)
+  }
+
+  return r;
+}
